ae0f9ff779752df12ce41cac3b365c0ce3a5d504
Brandon Saraiva <brandon.1610@hotmail.com>
2023-05-16 16:58:11 -0300

    Delete lotofacil directory

--- lotofacil/utility/pixel.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import time
-
-time.sleep(3)
-
-# Obter as coordenadas do mouse
-x, y = pyautogui.position()
-
-print(f"({x}, {y})                    ---------------------------------------------------------------------------------------------------")
